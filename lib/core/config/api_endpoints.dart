c7e4ef7f5b59495127d57a7357385b0c86d46217
SajaShawawra <202202357@bethlehem.edu>
2026-01-13 19:28:17 +0200

    edit

--- lib/core/config/api_endpoints.dart
+++ lib/core/config/api_endpoints.dart
@@ -11,22 +11,11 @@ class ApiEndpoints {
   static const String chats = '/chats';
   static const String messages = '/messages';
 
-  static const String dictionary = '/dictionary';
   static const String learning = '/learning';
   static const String smartPrediction = '/predictions';
 
-<<<<<<< HEAD
   static const String reports = '/reports';
   static const String adminReports = '/admin/reports';
-=======
-  static const String chats = '/v1/chats';
-  static const String messages = '/v1/messages';
+
   static const String dictionary = '/visual-dictionary';
-
-  static const String learning = '/v1/learning';
-  static const String smartPrediction = '/v1/predictions';
-
-  static const String reports = '/v1/reports';
-  static const String adminReports = '/v1/admin/reports';
->>>>>>> 8fe1ec9... feat : dictionary
 }
