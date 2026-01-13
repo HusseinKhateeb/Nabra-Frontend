8bb58ba0b11b0b9464d4ded79cf5e630b799dff0
SajaShawawra <202202357@bethlehem.edu>
2026-01-13 19:36:56 +0200

    edit

--- lib/core/router/app_routes.dart
+++ lib/core/router/app_routes.dart
@@ -1,16 +1,17 @@
 class AppRoutes {
-  static const splash = '/';
+  static const welcome = '/';
+  static const permissions = '/permissions';
+  static const splash = '/splash';
   static const login = '/login';
   static const register = '/register';
   static const home = '/home';
-
+  static const instructions = '/instructions';
   static const lipReading = '/lip-reading';
   static const sessions = '/sessions';
-  static const chat = '/chat';
+  static const chats = '/chats';
   static const dictionary = '/dictionary';
   static const learning = '/learning';
   static const admin = '/admin';
-
   static const favorites = '/favorites';
 
   // 🔐 Auth – Forgot password flow
