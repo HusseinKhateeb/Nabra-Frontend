5e303b477d113897bc005d47b8e48fd9d59fff4b
HusseinKhateeb <202201626@bethlehem.edu>
2026-03-02 16:54:18 +0200

    fix: Update API base URL for testing on real devices

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -3,6 +3,8 @@ class AppConfig {
   /// iOS simulator: use http://localhost:8080/api
   static const String apiBaseUrl = String.fromEnvironment(
     'NABRA_API_BASE_URL',
-    defaultValue: 'http://192.168.1.3:8080/api', // Use PC Wi-Fi IP for real device testing
+    defaultValue: 'http://192.168.1.2:8080/api', // Use PC Wi-Fi IP for real device testing
+    //defaultValue: 'http://10.0.2.2:8080/api', // Android emulator
+
   );
 }
